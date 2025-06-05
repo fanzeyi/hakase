feb029002b58f72970b6bbad357470df8288f30a
Zeyi (Rice) Fan <github@zeyi.fan>
2025-06-05 00:50:16 -0700

    upgrade clap

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,52 @@
-#[macro_use]
-extern crate clap;
-extern crate hakase;
-extern crate simplelog;
-
-use clap::{App, Arg};
-use simplelog::{TermLogger, LevelFilter, Config};
+use clap::Parser;
 use hakase::config;
+use simplelog::{Config, LevelFilter, TermLogger};
+
+#[derive(Parser, Debug)]
+#[command(name = "Hakase")]
+#[command(version = "0.0.1")]
+#[command(author = "Zeyi Fan <i@zr.is>")]
+#[command(about = "A URL shorter.", long_about = None)]
+struct Args {
+    /// Host to listen on
+    #[arg(short, long, default_value = "127.0.0.1")]
+    host: String,
+
+    /// Port to listen on
+    #[arg(short, long, default_value_t = 4000)]
+    port: u16,
+
+    /// Number of threads
+    #[arg(short, long, default_value_t = 8)]
+    thread: usize,
+
+    /// Database connection URL
+    #[arg(long, required = true)]
+    database: String,
+
+    /// Enable debug mode
+    #[arg(short, long)]
+    debug: bool,
+
+    /// Secret key for signing
+    #[arg(short, long)]
+    secret: Option<String>,
+}
 
 fn main() {
-    let matches = App::new("Hakase")
-        .version("0.0.1")
-        .author("Zeyi Fan <i@zr.is>")
-        .about("A URL shorter.")
-        .arg(Arg::with_name("host")
-             .short("h")
-             .long("host")
-             .value_name("HOST")
-             .default_value("127.0.0.1"))
-        .arg(Arg::with_name("port")
-             .short("p")
-             .long("port")
-             .value_name("PORT")
-             .default_value("4000"))
-        .arg(Arg::with_name("thread")
-             .short("t")
-             .long("thread")
-             .value_name("NUM")
-             .default_value("8"))
-        .arg(Arg::with_name("database")
-             .long("database")
-             .value_name("URL")
-             .required(true))
-        .arg(Arg::with_name("debug")
-             .short("d")
-             .long("debug"))
-        .arg(Arg::with_name("secret")
-             .short("s")
-             .long("secret")
-             .value_name("SECRET"))
-        .get_matches();
+    let args = Args::parse();
 
-    let host = matches.value_of("host").unwrap();
-    let port = value_t!(matches, "port", u16).unwrap();
-    let thread = value_t!(matches, "thread", usize).unwrap();
-
-    if matches.is_present("debug") {
+    if args.debug {
         let _ = TermLogger::init(LevelFilter::Debug, Config::default());
     } else {
         let _ = TermLogger::init(LevelFilter::Error, Config::default());
     }
 
-    let database_url = matches.value_of("database").unwrap();
-
-    let config = config::Config::new(
-        matches.value_of("secret").map(str::to_string),
-        database_url.to_string(),
-    );
+    let host = &args.host;
+    let port = args.port;
+    let thread = args.thread;
+    let database_url = args.database.clone();
+    let config = config::Config::new(args.secret.clone(), database_url);
 
     hakase::run(host, port, thread, config);
 }
